d2f0fff96cbd324aea118f519071f3458843217a
“Aiden <adnrdmnd@gmail.com>
2023-05-14 10:52:11 -0400

    DOM stuff with hours

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-fetchTotalHours();
+document.addEventListener("DOMContentLoaded", (event) => {
+    fetchTotalHours();
+});
 
 const RED = "FF5A5F";
 const BLUE = "5C9DFF";
@@ -17,29 +19,6 @@ async function fetchTotalHours() {
         return data;
     } catch(error) {
         console.log(error);
-    }
-}
-
-function darkMode() {
-    let body = document.body
-    let bodyColor = window.getComputedStyle(body).backgroundColor;
-
-    if (bodyColor.includes(42)) {
-        return true;
-    }
-    return false;
-}
-
-function modeChange() {
-    if (darkMode()) {
-        console.log("black to white");
-        //turn the background white
-        //turn all the white text black
-    }
-    else {
-        console.log("white to black");
-        //turn the background black
-        //turn all the black text white
     }
 }
 
